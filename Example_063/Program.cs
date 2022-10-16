e0af8269dd8e92e71fcc3eb9e198e7483bb6d998
borodov.m <borodov.m@gmail.com>
2022-10-16 23:26:56 +0300

    Added example 063

---
+++ Example_063/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Random random = new Random();
+int n = random.Next(5,11);
+Console.WriteLine($"n = {n}");
+
+int counter = 1;
+Recurse(n, counter);
+
+void Recurse(int n, int counter)
+{
+    Console.WriteLine($"n = {counter}");
+    if (counter >= n)
+    {
+        return;
+    }
+    counter++;
+    Recurse(n, counter);
+}
