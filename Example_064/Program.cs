fdda4fa61df1788aee0ccdedeed1c8764b43ddb5
borodov.m <borodov.m@gmail.com>
2022-10-17 00:57:00 +0300

    Added example 64

---
+++ Example_064/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N. Напишите рекурсивный метод, который
+// выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+// M = 1; N = 9. -> "3, 6, 9"
+// M = 13; N = 20. -> "15, 18"
+
+Random random = new Random();
+int M = random.Next(5,11);
+int N = random.Next(M,21);
+Console.WriteLine($"M = {M}, N = {N}");
+
+Recurse(M,N);
+
+void Recurse(int M, int N)
+{
+    if (M%3 == 0)
+    {
+        Console.WriteLine($"M = {M}");
+    }
+    if (M >= N)
+    {
+        return;
+    }
+    M++;
+    Recurse(M, N);
+}
